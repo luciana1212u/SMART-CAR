850d4939ceef9197b05e6990d3d2d384e9e1ac8f
Sebastian Obando <sebas22febrero@gmail.com>
2026-09-11 12:38:32 -0500

    creacion de la estructura en srping boot para el backend: sobando 4526

---
+++ backend/src/test/java/com/smartcar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
